9bf52ebf7032433073b7d04e3b84917c408c8318
Dusan Mesi <dusan.mesi@gmail.com>
2021-12-06 14:14:25 +0100

    Custom event

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -7,11 +7,8 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   
-@Input('abcde') element: {type:string, name:string,content:string}; 
-
-
+@Input('abcd') element: {type:string, name:string,content:string}; 
   constructor() { }
-
   ngOnInit(): void {
   }
 
